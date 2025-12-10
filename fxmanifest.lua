1c8d54bb0054da2b44e6074b2ff5ec5efb9f87f8
Mist the Goat <62227356+Mistextreme@users.noreply.github.com>
2025-12-10 16:45:20 +0000

    Upload: fxmanifest.lua

---
+++ fxmanifest.lua
@@ -0,0 +1,24 @@
+fx_version 'cerulean'
+game 'gta5'
+lua54 'yes'
+
+shared_scripts {
+    '@es_extended/imports.lua',
+    '@ox_lib/init.lua',
+    'config.lua'
+}
+
+client_script 'client/*.lua'
+
+ui_page 'ui/index.html'
+
+files {
+    'ui/index.html',
+    'ui/style.css',
+    'ui/script.js'
+}
+
+escrow_ignore {
+    'config.lua'
+}
+dependency '/assetpacks'
